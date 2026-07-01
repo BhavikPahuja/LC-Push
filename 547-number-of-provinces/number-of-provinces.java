7b6e55b4c7055ad11157177b9069f7d688d2b7f0
Bhavik Pahuja <bhavikpahuja0@gmail.com>
2026-07-01 10:25:58 +0530

    Time: 2 ms (43.37%) | Memory: 47 MB (87.69%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,24 +1,29 @@
 class DisjointSet {
-    private int size[];
-    private int parent[];
+
+    int parent[];
+    int size[];
 
     public DisjointSet(int n) {
-        size = new int[n + 1];
         parent = new int[n + 1];
+        size = new int[n + 1];
+
         for (int i=0; i<=n; i++) {
-            size[i] = 0;
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
     public int findParent(int node) {
-        if (node == parent[node]) {
+
+        if (parent[node] == node) {
             return node;
         }
+
         return parent[node] = findParent(parent[node]);
     }
 
     public void unionBySize(int u, int v) {
+
         int parent_u = findParent(u);
         int parent_v = findParent(v);
 
@@ -26,21 +31,22 @@ class DisjointSet {
             return;
         }
 
-        if (size[parent_u] < size[parent_v]) {
+        if (size[u] < size[v]) {
             parent[parent_u] = parent_v;
-            size[parent_v] = size[parent_u] + size[parent_v];
+            size[parent_v] += size[parent_u];
         } else {
             parent[parent_v] = parent_u;
-            size[parent_u] = size[parent_u] + size[parent_v];
+            size[parent_u] += size[parent_v];
         }
     }
 }
 
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+
         int n = isConnected.length;
         DisjointSet ds = new DisjointSet(n);
-        
+
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 if (isConnected[i][j] == 1) {
@@ -49,13 +55,13 @@ class Solution {
             }
         }
 
-        int count = 0;
+        int ans = 0;
         for (int i=0; i<n; i++) {
-            if (ds.findParent(i) == i) {
-                count++;
+            if (ds.parent[i] == i) {
+                ans++;
             }
         }
 
-        return count;
+        return ans;
     }
 }

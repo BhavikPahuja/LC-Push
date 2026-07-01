28e872374235aa63ac8abc64a47cc69260efacc7
Bhavik Pahuja <bhavikpahuja0@gmail.com>
2026-07-01 11:58:22 +0530

    Time: 29 ms (79.56%) | Memory: 49.5 MB (68.20%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,82 +1,105 @@
 class DisjointSet {
-    private int size[];
-    private int parent[];
+
+    int parent[];
+    int size[];
 
     public DisjointSet(int n) {
-        size = new int[n + 1];
-        parent = new int[n + 1];
 
-        for (int i=0; i<=n; i++) {
-            size[i] = 0;
+        parent = new int[n + 1];
+        size = new int[n + 1];
+
+        for (int i=0; i<n; i++) {
+
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
     public int findParent(int node) {
+
         if (parent[node] == node) {
+            
             return node;
         }
+
         return parent[node] = findParent(parent[node]);
     }
 
     public void unionBySize(int u, int v) {
+
         int parent_u = findParent(u);
         int parent_v = findParent(v);
 
         if (parent_u == parent_v) {
+            
             return;
         }
 
         if (size[parent_u] < size[parent_v]) {
+            
             parent[parent_u] = parent_v;
-            size[parent_v] = size[parent_u] + size[parent_v];
+            size[parent_v] += size[parent_u];
         } else {
+
             parent[parent_v] = parent_u;
-            size[parent_u] = size[parent_u] + size[parent_v];
+            size[parent_u] += size[parent_v];
         }
     }
 }
 
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        
         int n = accounts.size();
+
         DisjointSet ds = new DisjointSet(n);
-        Map<String, Integer> mapMail = new HashMap<>();
+
+        Map<String, Integer> mailMap = new HashMap<>();
+
         for (int i=0; i<n; i++) {
+            
             for (int j=1; j<accounts.get(i).size(); j++) {
+                
                 String mail = accounts.get(i).get(j);
-                if (!mapMail.containsKey(mail)) {
-                    mapMail.put(mail, i);
+                if (mailMap.containsKey(mail)) {
+                    
+                    ds.unionBySize(i, mailMap.get(mail));
                 } else {
-                    ds.unionBySize(i, mapMail.get(mail));
+                    
+                    mailMap.put(mail, i);
                 }
             }
         }
-        
-        List<String> mergedMail[] = new ArrayList[n];
+
+        List<List<String>> temp = new ArrayList<>();
         for (int i=0; i<n; i++) {
-            mergedMail[i] = new ArrayList<String>();
+            
+            temp.add(new ArrayList<>());
         }
-        
-        for (Map.Entry<String, Integer> mails : mapMail.entrySet()) {
-            String mail = mails.getKey();
-            int node = ds.findParent(mails.getValue());
-            mergedMail[node].add(mail);
+
+        for (Map.Entry<String, Integer> entry : mailMap.entrySet()) {
+
+            String mail = entry.getKey();
+            int node = ds.findParent(entry.getValue());
+            temp.get(node).add(mail);
         }
 
         List<List<String>> ans = new ArrayList<>();
+
         for (int i=0; i<n; i++) {
-            if (mergedMail[i].size() == 0) {
-                continue;
+
+            if (temp.get(i).size() != 0) {
+
+                Collections.sort(temp.get(i));
+
+                List<String> group = new ArrayList<>();
+                group.add(accounts.get(i).get(0));
+                group.addAll(temp.get(i));
+
+                ans.add(group);
             }
-            Collections.sort(mergedMail[i]);
-            List<String> temp = new ArrayList<>();
-            temp.add(accounts.get(i).get(0));
-            for (String mail : mergedMail[i]) {
-                temp.add(mail);
-            }
-            ans.add(temp);
         }
+
         return ans;
     }
 }
